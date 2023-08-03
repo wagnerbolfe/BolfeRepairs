e00840c7decbe375ac5aa181d874181da479b8da
Wagner Bolfe <shaper@live.com>
2023-08-03 16:53:04 -0300

    Added a Zod Validation

--- clientapp/src/pages/client/create.tsx
+++ clientapp/src/pages/client/create.tsx
@@ -1,17 +1,32 @@
 
 import { Header } from "@/components/Header";
 import { Sidebar } from "@/components/Sidebar";
-import { Box, Button, Divider, Flex, HStack, Heading, SimpleGrid, VStack, Checkbox, FormControl, Input, FormLabel, FormErrorIcon, FormErrorMessage } from "@chakra-ui/react";
+import { Box, Button, Divider, Flex, HStack, Heading, SimpleGrid, VStack, Checkbox, FormControl, Input, FormLabel, Text } from "@chakra-ui/react";
 import Link from "next/link";
 import { useForm } from 'react-hook-form'
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
+const claimClientFormSchema = z.object({
+  name: z.string(),
+  street: z.string(),
+  city: z.string(),
+  neighborhood: z.string(),
+  uf: z.string().max(2, { message: '* O estado precisa de duas letras' }).transform(uf => uf.toUpperCase()),
+  phone: z.string().regex(/^1\d\d(\d\d)?$|^0800 ?\d{3} ?\d{4}$|^(\(0?([1-9a-zA-Z][0-9a-zA-Z])?[1-9]\d\) ?|0?([1-9a-zA-Z][0-9a-zA-Z])?[1-9]\d[ .-]?)?(9|9[ .-])?[2-9]\d{3}[ .-]?\d{4}$/gm, 'Digite um telefone válido'),
+  mobile: z.string().regex(/^1\d\d(\d\d)?$|^0800 ?\d{3} ?\d{4}$|^(\(0?([1-9a-zA-Z][0-9a-zA-Z])?[1-9]\d\) ?|0?([1-9a-zA-Z][0-9a-zA-Z])?[1-9]\d[ .-]?)?(9|9[ .-])?[2-9]\d{3}[ .-]?\d{4}$/gm, 'Digite um celular válido'),
+  details: z.string(),
+  situation: z.boolean()
+})
+
+type ClaimClientFormData = z.infer<typeof claimClientFormSchema>
 
 export default function CreateClient() {
-  const { handleSubmit, register } = useForm()
+  const { handleSubmit, register, formState: { errors } } = useForm<ClaimClientFormData>({
+    resolver: zodResolver(claimClientFormSchema)
+  })
 
-  function handlePreRegister(data: any) {
+  async function handlePreRegister(data: ClaimClientFormData) {
     console.log(data)
   }
 
@@ -80,6 +95,7 @@ export default function CreateClient() {
                     {...register('neighborhood')}
                   />
                 </FormControl>
+
                 <FormControl>
                   <FormLabel>UF</FormLabel>
                   <Input
@@ -90,7 +106,9 @@ export default function CreateClient() {
                     size="lg"
                     {...register('uf')}
                   />
+                  <Text p={2} fontSize="xs">{errors.uf && errors.uf.message}</Text>
                 </FormControl>
+
               </SimpleGrid>
 
               <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
@@ -102,9 +120,12 @@ export default function CreateClient() {
                     variant="filled"
                     _hover={{ bgColor: "gray.900" }}
                     size="lg"
+                    type="number"
                     {...register('phone')}
                   />
+                  <Text p={2} fontSize="xs">{errors.phone && errors.phone.message}</Text>
                 </FormControl>
+
                 <FormControl>
                   <FormLabel>Celular</FormLabel>
                   <Input
@@ -113,8 +134,10 @@ export default function CreateClient() {
                     variant="filled"
                     _hover={{ bgColor: "gray.900" }}
                     size="lg"
+                    type="number"
                     {...register('mobile')}
                   />
+                  <Text p={2} fontSize="xs">{errors.mobile && errors.mobile.message}</Text>
                 </FormControl>
               </SimpleGrid>
 
